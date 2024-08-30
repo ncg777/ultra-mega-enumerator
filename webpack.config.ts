d0ec9eed5af2fbedb1cca8c95292c9dba38623de
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-08-29 22:52:51 -0400

    Fixed a project setup error

--- webpack.config.ts
+++ webpack.config.ts
@@ -7,7 +7,7 @@ const config: Configuration = {
   output: {
     filename: 'index.js', // Output JavaScript file
     path: path.resolve(__dirname, 'dist'), // Output directory
-    library: 'enumerati', // If you want to expose your library
+    library: 'ultra-mega-enumerator', // If you want to expose your library
     libraryTarget: 'umd', // Module format
     globalObject: 'this',
   },
